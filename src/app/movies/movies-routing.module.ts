e3b723272d8a7dac2fb9bced6a613874c2fb8ec0
aaronshivers <aaron@moonmail.org>
2020-02-02 07:11:36 -0600

    redirect / to /movies

--- src/app/movies/movies-routing.module.ts
+++ src/app/movies/movies-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { MoviesComponent } from './movies.component';
 
 const routes: Routes = [
-  { path: '', component: MoviesComponent },
+  { path: 'movies', component: MoviesComponent },
 ];
 
 @NgModule({
